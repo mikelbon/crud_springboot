7c2bb1f12455d1f02cfaac53d2fafcc358abef55
Miguel Carhuas <mikel_ct@hotmail.com>
2024-04-12 10:36:30 -0500

    primer commit

---
+++ src/main/java/com/crud/app/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.crud.app.repository;
+
+import com.crud.app.entity.Department;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends CrudRepository<Department, Long> {
+}
---
+++ src/main/java/com/crud/app/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.crud.app.service;
+
+import com.crud.app.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    // Save operation
+    Department saveDepartment(Department department);
+
+    // Read operation
+    List<Department> fetchDepartmentList();
+
+    // Update operation
+    Department updateDepartment(Department department,
+                                Long departmentId);
+
+    // Delete operation
+    void deleteDepartmentById(Long departmentId);
+}
